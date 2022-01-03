8b88e1e46df412e939a1a7bcfb055815c586bbe5
imaphatduc <vutranducphat@gmail.com>
2022-01-03 22:14:26 +0700

    Move TS source code to src/

--- src/animations/animation.js
+++
@@ -1,9 +0,0 @@
-import * as d3 from "d3";
-
-export class Animation {
-    constructor({ cubicon, duration, ease = d3.easeCubic }) {
-        this.cubicon = cubicon;
-        this.duration = duration;
-        this.ease = ease;
-    }
-}
