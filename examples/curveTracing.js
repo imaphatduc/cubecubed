8d5e1b5193c41c0b3c922394e799ebfcd5daffe3
imaphatduc <vutranducphat@gmail.com>
2022-01-18 18:58:11 +0700

    Move code to function

--- examples/curveTracing.js
+++ examples/curveTracing.js
@@ -3,7 +3,6 @@ import {
     Group,
     Vector2,
     COLOR,
-    Create,
     Grid,
     DrawGridFromScreenSides,
     ParamatricCurve,
@@ -13,42 +12,51 @@ import {
     Write,
 } from "../src/index";
 
-const scene = new Scene("playground");
-const group = new Group("group", scene);
+/// This variable keeps track of the time goes by during the animations
+/// We'll use this to control time, so don't forget to include it to your code
+/// In this example, the scene has only one group, so we don't need `elapsed`
+let elapsed = 0;
 
-const grid = new Grid({ group: group }).render();
-group.play([new DrawGridFromScreenSides(grid)]);
+function curveTracing() {
+    const scene = new Scene("playground");
+    const group = new Group("group", scene);
 
-const curve = new ParamatricCurve({
-    group: group,
-    tRange: [0, 10],
-    functionDef: (t) => new Vector2(t * Math.cos(t), t * Math.sin(t)),
-    CONFIG: {
-        lineColor: COLOR.CYAN,
-        lineWidth: 1.5,
-    },
-}).render();
+    const grid = new Grid({ group: group }).render();
+    group.play([new DrawGridFromScreenSides(grid)]);
 
-const drawer = new Line({
-    group: group,
-    endPoint: curve.getFirstOutputPoint(),
-    CONFIG: {
-        lineColor: COLOR.PINK_1,
-    },
-}).render();
+    const curve = new ParamatricCurve({
+        group: group,
+        tRange: [0, 10],
+        functionDef: (t) => new Vector2(t * Math.cos(t), t * Math.sin(t)),
+        CONFIG: {
+            lineColor: COLOR.CYAN,
+            lineWidth: 1.5,
+        },
+    }).render();
 
-group.play([
-    new Trace({
-        cubicon: drawer,
-        curve: curve,
-        duration: 5000,
-    }),
-]);
+    const drawer = new Line({
+        group: group,
+        endPoint: curve.getFirstOutputPoint(),
+        CONFIG: {
+            lineColor: COLOR.PINK_1,
+        },
+    }).render();
 
-const tex = new MathText({
-    group: group,
-    position: new Vector2(8, 5),
-    text: "f(t) = \\begin{bmatrix} t \\cos(t) \\\\ t \\sin(t) \\end{bmatrix}",
-}).render();
+    group.play([
+        new Trace({
+            cubicon: drawer,
+            curve: curve,
+            duration: 5000,
+        }),
+    ]);
 
-group.play([new Write({ cubicon: tex })]);
+    const tex = new MathText({
+        group: group,
+        position: new Vector2(8, 5),
+        text: "f(t) = \\begin{bmatrix} t \\cos(t) \\\\ t \\sin(t) \\end{bmatrix}",
+    }).render();
+
+    group.play([new Write({ cubicon: tex })]);
+}
+
+curveTracing();
