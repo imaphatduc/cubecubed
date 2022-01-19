d0d90b7b47cee587d81c4951f0ecad85a7917943
imaphatduc <vutranducphat@gmail.com>
2022-01-19 22:54:14 +0700

    Add example file for vector field visualization

---
+++ examples/vectorField.js
@@ -0,0 +1,26 @@
+import {
+    Group,
+    Scene,
+    Vector2,
+    VectorField,
+    Grid,
+    DrawGridFromScreenSides,
+} from "../src/index";
+
+function vectorField() {
+    const scene = new Scene("visualize-2d-vector-field");
+
+    const group = new Group("vector-field", scene);
+
+    const grid = new Grid({ group: group }).render();
+    group.play([new DrawGridFromScreenSides(grid)]);
+
+    const vectorField = new VectorField({
+        group: group,
+        functionDef: (pos) => new Vector2(Math.sin(pos.y), Math.sin(pos.x)),
+    });
+
+    vectorField.createVectors();
+}
+
+vectorField();
