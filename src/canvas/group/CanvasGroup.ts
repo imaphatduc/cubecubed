99eae1351a116c9441751a71d1898a424d87e294
imaphatduc <vutranducphat@gmail.com>
2022-03-27 19:08:50 +0700

    Define bounds & convert functions for `CanvasGroup()`

--- src/canvas/group/CanvasGroup.ts
+++ src/canvas/group/CanvasGroup.ts
@@ -1,7 +1,13 @@
+import { ScaleLinear, scaleLinear } from "d3-scale";
+
 import p5, { Renderer } from "p5";
-import { CanvasAnimation } from "../animations/CanvasAnimation";
-import { CanvasCubicon } from "../cubicons/CanvasCubicon";
-import { Scene } from "../../scene/Scene";
+//+++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+import { Scene } from "@scene/Scene";
+
+import { CanvasAnimation } from "@animations/CanvasAnimation";
+
+import { CanvasCubicon } from "@cubicons/CanvasCubicon";
 
 /**
  * The dad/mom object of every pack of objects in the visualization.
@@ -40,6 +46,56 @@ export class CanvasGroup {
     animations: CanvasAnimation[] = [];
 
     /**
+     * Number of squares in the x direction.
+     */
+    xSquareNums: number;
+
+    /**
+     * Number of squares in the x direction.
+     */
+    ySquareNums: number;
+
+    /**
+     * Length of a square in this scene.
+     */
+    squareLength = 40;
+
+    /**
+     * Ratio between square length in x direction and y direction.
+     */
+    ratio: [number, number] = [1, 1];
+
+    /**
+     * x coordinate bound values of this scene.
+     */
+    xBound: [number, number];
+
+    /**
+     * y coordinate bound values of this scene.
+     */
+    yBound: [number, number];
+
+    /**
+     * Convert x value of grid coordinates to real-world coordinates.
+     */
+    xGtoW: ScaleLinear<number, number, never>;
+
+    /**
+     * Convert y value of grid coordinates to real-world coordinates.
+     */
+    yGtoW: ScaleLinear<number, number, never>;
+
+    /**
+     * Convert x value of real-world coordinates to grid coordinates.
+     */
+    xWtoG: ScaleLinear<number, number, never>;
+
+    /**
+     * Convert y value of real-world coordinates to grid coordinates.
+     */
+    yWtoG: ScaleLinear<number, number, never>;
+
+    /**
      * Include this group to HTML flow.
      *
      * @param groupName Name of the group.
@@ -48,6 +104,10 @@ export class CanvasGroup {
      */
     constructor(groupName: string, scene: Scene) {
         this.scene = scene;
+
+        this.defineBoundsAndSquares(this.ratio);
+
+        this.defineCovertFunctions(this.ratio);
 
         const sketch = (p5: p5) => {
             p5.setup = () => {
@@ -69,6 +129,55 @@ export class CanvasGroup {
         new p5(sketch);
 
         this.name = groupName;
+    }
+
+    private defineBoundsAndSquares(ratio: [number, number]) {
+        const { sceneWidth, sceneHeight } = this.scene;
+
+        const xSquareLength = ratio[0] * this.squareLength;
+        const ySquareLength = ratio[1] * this.squareLength;
+
+        this.xSquareNums = Math.floor(sceneWidth / xSquareLength);
+        this.ySquareNums = Math.floor(sceneHeight / ySquareLength);
+
+        this.xBound = [
+            Math.floor(-this.xSquareNums / 2),
+            -Math.floor(-this.xSquareNums / 2),
+        ];
+        this.yBound = [
+            Math.floor(-this.ySquareNums / 2),
+            -Math.floor(-this.ySquareNums / 2),
+        ];
+    }
+
+    private defineCovertFunctions(ratio: [number, number]) {
+        const { sceneWidth, sceneHeight } = this.scene;
+
+        const xBound = [
+            -sceneWidth / (this.squareLength * ratio[0]),
+            sceneWidth / (this.squareLength * ratio[0]),
+        ];
+
+        const yBound = [
+            -sceneHeight / (this.squareLength * ratio[1]),
+            sceneHeight / (this.squareLength * ratio[1]),
+        ];
+
+        this.xGtoW = scaleLinear()
+            .domain(xBound)
+            .range([-sceneWidth, sceneWidth]);
+
+        this.yGtoW = scaleLinear()
+            .domain(yBound)
+            .range([-sceneHeight, sceneHeight]);
+
+        this.xWtoG = scaleLinear()
+            .domain([-sceneWidth, sceneWidth])
+            .range(this.xBound);
+
+        this.yWtoG = scaleLinear()
+            .domain([-sceneHeight, sceneHeight])
+            .range(this.yBound);
     }
 
     /**
