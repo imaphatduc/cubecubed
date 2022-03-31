f50b06747b50636abe583cb78a737e0570e3919f
imaphatduc <vutranducphat@gmail.com>
2022-03-31 08:38:34 +0700

    `scaleFactor` property for scaling canvas cubicon position

--- src/canvas/cubicons/CanvasCubicon.ts
+++ src/canvas/cubicons/CanvasCubicon.ts
@@ -17,20 +17,25 @@ export abstract class CanvasCubicon {
     position: Vector3;
 
     /**
+     * Scale the position vector of this cubicon by this number.
+     */
+    scaleFactor: number;
+
+    /**
      * Total time of all called animations (in milliseconds).
      */
     elapsedTime = 0;
 
-    constructor({
-        group,
-        position = new Vector3(0, 0, 0),
-    }: {
+    constructor(params: {
         group: CanvasGroup;
-        position: Vector3;
+        position?: Vector3;
+        scaleFactor?: number;
     }) {
-        this.group = group;
+        this.group = params.group;
 
-        this.position = position;
+        this.position = params.position ?? new Vector3(0, 0, 0);
+
+        this.scaleFactor = params.scaleFactor ?? 1;
 
         this.group.cubicons.push(this);
     }
