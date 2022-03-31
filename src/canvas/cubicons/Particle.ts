83b70d712a85d8c6934b2471dc27fadb6d4416a2
imaphatduc <vutranducphat@gmail.com>
2022-03-31 08:39:13 +0700

    `scaleFactor` property for scaling canvas cubicon position

--- src/canvas/cubicons/Particle.ts
+++ src/canvas/cubicons/Particle.ts
@@ -6,20 +6,30 @@ import { CanvasCubicon } from "./CanvasCubicon";
 export class Particle extends CanvasCubicon {
     readonly cubType = "particle";
 
-    constructor(params: { group: CanvasGroup; position: Vector3 }) {
+    constructor(params: {
+        group: CanvasGroup;
+        position?: Vector3;
+        scaleFactor?: number;
+    }) {
         super({
             group: params.group,
             position: params.position ?? new Vector3(0, 0, 0),
+            scaleFactor: params.scaleFactor ?? 1,
         });
     }
 
     render() {
         const { xGtoW, yGtoW } = this.group;
 
-        background(0, 10);
+        background(0);
 
         stroke(255);
+        strokeWeight(3);
 
-        point(xGtoW(this.position.x), yGtoW(this.position.y), this.position.z);
+        point(
+            xGtoW(this.position.x) * this.scaleFactor,
+            yGtoW(this.position.y) * this.scaleFactor,
+            this.position.z
+        );
     }
 }
