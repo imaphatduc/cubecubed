15c841d2970fe2eadb1a454765006829a11dc12b
imaphatduc <vutranducphat@gmail.com>
2022-04-03 00:44:40 +0700

    Defining cubicon types for playing canvas animations

--- src/canvas/animations/CanvasAnimation.ts
+++ src/canvas/animations/CanvasAnimation.ts
@@ -1,16 +1,20 @@
+import { FLOW_TYPES } from "./Flow";
+
+type CANVAS_ANIM_TYPES = FLOW_TYPES;
+
 export class CanvasAnimation {
     /**
      * The target cubicon of this animation.
      */
-    cubicon: any;
+    cubicon: CANVAS_ANIM_TYPES;
 
     /**
      * Time to play this animation (in milliseconds).
      */
     duration: number;
 
-    constructor(params: { cubicon: any; duration?: number | undefined }) {
+    constructor(params: { cubicon: CANVAS_ANIM_TYPES; duration?: number }) {
         this.cubicon = params.cubicon;
-        this.duration = params.duration || 0;
+        this.duration = params.duration ?? 0;
     }
 }
