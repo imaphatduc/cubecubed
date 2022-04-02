b9a613f2a4681c2707860bc45b424d4f63cb2454
imaphatduc <vutranducphat@gmail.com>
2022-04-03 02:11:11 +0700

    Change animation's cubicon types to `any` for easier type definitions

--- src/canvas/animations/CanvasAnimation.ts
+++ src/canvas/animations/CanvasAnimation.ts
@@ -1,19 +1,15 @@
-import { FLOW_TYPES } from "./Flow";
-
-type CANVAS_ANIM_TYPES = FLOW_TYPES;
-
 export class CanvasAnimation {
     /**
      * The target cubicon of this animation.
      */
-    cubicon: CANVAS_ANIM_TYPES;
+    cubicon: any;
 
     /**
      * Time to play this animation (in milliseconds).
      */
     duration: number;
 
-    constructor(params: { cubicon: CANVAS_ANIM_TYPES; duration?: number }) {
+    constructor(params: { cubicon: any; duration?: number }) {
         this.cubicon = params.cubicon;
         this.duration = params.duration ?? 0;
     }
