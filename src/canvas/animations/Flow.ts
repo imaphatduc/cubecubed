078083ae16fa130028d53011e58bd1ba180b9080
imaphatduc <vutranducphat@gmail.com>
2022-04-03 00:45:15 +0700

    Add comment docs

--- src/canvas/animations/Flow.ts
+++ src/canvas/animations/Flow.ts
@@ -4,11 +4,40 @@ import { CanvasAnimation } from "./CanvasAnimation";
 
 import { Particle } from "@cubicons/Particle";
 
+export type FLOW_TYPES = Particle;
+
 type TransformationFunction = (position: Vector3) => Vector3;
 
+/**
+ * Move a cubicon based on the specified function to create an invisible flow.
+ * The image of the last animation frame's cubicon will be removed from the
+ * scene.
+ *
+ * All images of the cubicon will be separated (not continuous).
+ */
 export class Flow extends CanvasAnimation {
     /**
-     * Transformation function for the cubicon's position.
+     * The function defining the position of the target cubicon.
+     * This function should take a `Vector3()` object as both input and output.
+     * The `Vector3()` output object is the cubicon's position at next frame.
+     *
+     * Example of a function defining particle flow in a vector field:
+     *
+     * ```
+     * // Specific delta time
+     * const dt = 0.01;
+     *
+     * const sineField = ({ x, y, z }: Vector3) => {
+     *     const dx = Math.sin(y);
+     *     const dy = Math.sin(x);
+     *
+     *     x += dx * dt;
+     *     y += dy * dt;
+     *
+     *     return new Vector3(x, y, z);
+     * }
+     * ```
+     *
      */
     functionDef: TransformationFunction;
 
@@ -16,7 +45,7 @@ export class Flow extends CanvasAnimation {
         /**
          * The target cubicon to play this animation.
          */
-        cubicon: Particle;
+        cubicon: FLOW_TYPES;
         /**
          * The differential equation to be applied.
          */
@@ -28,7 +57,7 @@ export class Flow extends CanvasAnimation {
     }) {
         super({
             cubicon: params.cubicon,
-            duration: params.duration,
+            duration: params.duration ?? 0,
         });
 
         this.functionDef = params.functionDef;
--- src/canvas/cubicons/Particle.ts
+++ src/canvas/cubicons/Particle.ts
@@ -13,22 +13,48 @@ export interface PARTICLE_CONFIG {
      */
     strokeWidth: number;
 }
+
 export const PARTICLE_DEFAULT_CONFIG: PARTICLE_CONFIG = {
     strokeColor: "#fff",
     strokeWidth: 2,
 };
 
+/**
+ * Creating a new `Particle()` object means render a point
+ * on the animation screen. Do not call `render()` method
+ * directly on any instance of this class, as it is handled
+ * automatically by the group object.
+ *
+ * Particle term here only represents a point (aka a dot),
+ * not a flow or anything like that.
+ */
 export class Particle extends CanvasCubicon {
     readonly cubType = "particle";
 
-    strokeColor: string;
+    /**
+     * Config options of the particle.
+     */
+    CONFIG: PARTICLE_CONFIG;
 
-    strokeWidth: number;
-
+    /**
+     * @param params An object that contains options to form the particle.
+     */
     constructor(params: {
+        /**
+         * The group that the particle belongs to
+         */
         group: CanvasGroup;
+        /**
+         * Position of the particle.
+         */
         position?: Vector3;
+        /**
+         * Scale the position vector of the particle by this number.
+         */
         scaleFactor?: number;
+        /**
+         * Config options of the particle.
+         */
         CONFIG?: PARTICLE_CONFIG;
     }) {
         super({
@@ -37,17 +63,23 @@ export class Particle extends CanvasCubicon {
             scaleFactor: params.scaleFactor ?? 1,
         });
 
-        ({
-            strokeColor: this.strokeColor = PARTICLE_DEFAULT_CONFIG.strokeColor,
-            strokeWidth: this.strokeWidth = PARTICLE_DEFAULT_CONFIG.strokeWidth,
-        } = params.CONFIG ?? PARTICLE_DEFAULT_CONFIG);
+        this.CONFIG = {
+            strokeColor:
+                params.CONFIG?.strokeColor ??
+                PARTICLE_DEFAULT_CONFIG.strokeColor,
+            strokeWidth:
+                params.CONFIG?.strokeWidth ??
+                PARTICLE_DEFAULT_CONFIG.strokeWidth,
+        };
     }
 
     render(p: any) {
         const { xGtoW, yGtoW, zGtoW } = this.group;
 
-        p.stroke(this.strokeColor);
-        p.strokeWeight(this.strokeWidth);
+        const { strokeColor, strokeWidth } = this.CONFIG;
+
+        p.stroke(strokeColor);
+        p.strokeWeight(strokeWidth);
 
         p.point(
             xGtoW(this.position.x) * this.scaleFactor,
