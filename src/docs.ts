2b6e704984f66b954d7d5a90f85ba2c34b4c6fd5
imaphatduc <vutranducphat@gmail.com>
2022-04-08 20:13:51 +0700

    Entry point for docs

---
+++ src/docs.ts
@@ -0,0 +1,34 @@
+export * from "./index";
+
+export { EASE_TYPE } from "@consts";
+
+export { SCENE_CONFIG } from "@scene/Scene";
+
+// Groups
+export { ANIMATIONS_INFO } from "@group/CanvasGroup";
+
+// Cubicons
+export { VECTOR_FIELD_CONFIG } from "@cubicons/VectorField";
+
+export { PARTICLE_CONFIG } from "@cubicons/Particle";
+
+export { STREAMLINE_CONFIG } from "@cubicons/StreamLine";
+
+/// Geometry cubicons
+export {
+    SHAPE_CONFIG,
+    LINE_CONFIG,
+    VECTOR_CONFIG,
+} from "@cubicons/geometry/Geometry";
+
+export { RECT_GRID_DATA } from "@cubicons/geometry/Rectangle";
+
+/// Coordinate system cubicons
+export {
+    PT_ON_GRAPH_DATA,
+    PT_TO_COORDS_DATA,
+    AXES_CONFIG,
+} from "@cubicons/coordinate-system/Axes";
+
+// Animations
+export { CREATE_TYPES, FADE_TYPES } from "@animations/Animation";
