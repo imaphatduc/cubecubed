ef557e31882f0d334ce938a1dfda5da1cda7d5f5
imaphatduc <vutranducphat@gmail.com>
2022-04-09 21:36:32 +0700

    Update init scene

--- init/example.js
+++ init/example.js
@@ -11,80 +11,63 @@ import {
     Vector2,
 } from "cubecubed";
 
-/// This variable keeps track of the time goes by during the animations
-/// We'll use this to control time, so don't forget to include it to your code
-let elapsed = 0;
-
-/// By convention, each function implements the animations in each scene...
+// By convention, first-class functions represent scenes.
 function drawShapes() {
     const scene = new Scene("draw-shapes");
 
-    /// ...And groups should be inside functions
+    // Groups are defined inside their scene function.
     function squares() {
-        const squares = new Group("squares", scene);
+        const group = new Group("squares", scene);
 
         const square1 = new Square({
-            group: squares,
+            group: group,
             sideLength: 2,
             CONFIG: {
                 strokeColor: COLOR.PINK_1,
             },
         }).render();
-
-        squares.play([new Create({ cubicon: square1 })]);
 
         const square2 = new Square({
-            group: squares,
+            group: group,
             sideLength: 2,
             CONFIG: {
                 strokeColor: COLOR.PINK_1,
             },
         }).render();
 
-        squares.play([
+        group.play([new Create({ cubicon: square1 })]);
+
+        group.play([
             new Create({ cubicon: square2 }),
             new Rotate({ cubicon: square1, degree: 45 }),
         ]);
-
-        /// Set elapsed
-        elapsed = squares.groupElapsed;
     }
 
     function vectors() {
-        const vectors = new Group("vectors", scene);
+        const group = new Group("vectors", scene);
 
         const vector = new Vector({
-            group: vectors,
+            group: group,
             endPoint: new Vector2(4, 4),
             CONFIG: {
                 lineColor: COLOR.TEAL_1,
             },
         }).render();
 
-        /// wait for the previous group's animations to complete, then start this group's ones
-        vectors.sleep(elapsed);
+        group.play([new Create({ cubicon: vector })]);
+    }
 
-        vectors.play([new Create({ cubicon: vector })]);
+    function planeGrid() {
+        const group = new Group("plane-grid-group", scene);
 
-        elapsed = vectors.groupElapsed;
+        const grid = new Grid({ group: group }).render();
+
+        group.play([new DrawGridFromScreenSides(grid)]);
     }
 
     squares();
     vectors();
-}
-
-function animatePlaneGrid() {
-    const scene = new Scene("animate-plane-grid");
-
-    const planeGridGroup = new Group("plane-grid-group", scene);
-
-    const grid = new Grid({ group: planeGridGroup }).render();
-
-    planeGridGroup.sleep(elapsed - 2000);
-    planeGridGroup.play([new DrawGridFromScreenSides(grid)]);
-
-    elapsed = planeGridGroup.groupElapsed;
+    planeGrid();
 }
 
 drawShapes();
-animatePlaneGrid();
