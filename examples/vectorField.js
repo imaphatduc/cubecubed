066cbe14d4d710138dedb72ed54144fbaaed6d52
imaphatduc <vutranducphat@gmail.com>
2022-04-09 20:23:15 +0700

    Update `vectorField` example

--- examples/vectorField.js
+++ examples/vectorField.js
@@ -1,3 +1,4 @@
+import { range } from "d3";
 import {
     Group,
     Scene,
@@ -6,26 +7,84 @@ import {
     Grid,
     DrawGridFromScreenSides,
     DrawVectorField,
+    CanvasGroup,
+    StreamLine,
+    Vector3,
+    SimulateStream,
 } from "../src/index";
 
-function vectorField() {
+const dt = 0.1;
+
+function vectorFieldSimulation() {
     const scene = new Scene("visualize-2d-vector-field");
 
-    const group = new Group("vector-field", scene);
+    const sineField = ({ x, y, z }) => {
+        const dx = Math.sin(y);
+        const dy = Math.sin(x);
 
-    const grid = new Grid({ group: group }).render();
-    group.play([new DrawGridFromScreenSides(grid)]);
+        x += dx * dt;
+        y += dy * dt;
 
-    const vectorField = new VectorField({
-        group: group,
-        functionDef: (pos) => new Vector2(Math.sin(pos.y), Math.sin(pos.x)),
-        CONFIG: {
-            isColored: true,
-            isScaled: true,
-        },
-    });
+        return new Vector3(x, y, z);
+    };
 
-    group.play([new DrawVectorField({ cubicon: vectorField })]);
+    function vectorField() {
+        const group = new Group("vector-field", scene);
+
+        const grid = new Grid({ group: group }).render();
+        group.play([new DrawGridFromScreenSides(grid)]);
+
+        const vectorField = new VectorField({
+            group: group,
+            functionDef: (pos) => new Vector2(Math.sin(pos.y), Math.sin(pos.x)),
+            CONFIG: {
+                isScaled: true,
+                lineColor: "scaled",
+                lineWidth: 1,
+            },
+        });
+
+        group.play([new DrawVectorField({ cubicon: vectorField })]);
+    }
+
+    function flowField() {
+        const group = new CanvasGroup("flow-field", scene);
+
+        const { xBound, yBound } = group;
+
+        const stream = [];
+
+        range(...xBound, 1).forEach((x) => {
+            range(...yBound, 1).forEach((y) => {
+                stream.push(
+                    new StreamLine({
+                        group: group,
+                        position: new Vector3(x, y, 0),
+                        functionDef: sineField,
+                        maxVertices: 20,
+                        CONFIG: {
+                            strokeColor: "#5e2eff",
+                            strokeWidth: 2,
+                        },
+                    })
+                );
+            });
+        });
+
+        const streamAnimations = stream.map((streamline) => {
+            streamline.vertices.splice(1);
+
+            return new SimulateStream({
+                cubicon: streamline,
+                duration: 10000,
+            });
+        });
+
+        group.play(streamAnimations);
+    }
+
+    vectorField();
+    flowField();
 }
 
-vectorField();
+vectorFieldSimulation();
