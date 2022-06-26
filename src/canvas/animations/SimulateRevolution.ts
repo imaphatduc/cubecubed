8b880b26fb4a9cf37dae0a25fd0dd3a785cf9666
imaphatduc <vutranducphat@gmail.com>
2022-06-26 23:51:04 +0700

    refactor: `SimulateRevolution` class

--- src/canvas/animations/SimulateRevolution.ts
+++ src/canvas/animations/SimulateRevolution.ts
@@ -1,6 +1,3 @@
-import { range } from "d3-array";
-//+++++++++++++++++++++++++++++++++++++++++++++++++++//
-
 import { CanvasAnimation } from "./CanvasAnimation";
 
 import { RevolutionSurface } from "@cubicons/RevolutionSurface";
@@ -32,26 +29,11 @@ export class SimulateRevolution extends CanvasAnimation {
     }
 
     private simulateRevolution(t: number, cubicon: RevolutionSurface) {
-        const { widthSegments, heightSegments } = cubicon;
-
         const vertices = cubicon.geometry.attributes.position;
 
         const tRatio = t / (this.duration / 1000);
 
-        range(0, vertices.count, widthSegments).forEach((i) => {
-            const theta =
-                (2 * Math.PI * i) / (widthSegments + 1) / (heightSegments - 1);
-
-            range(i, i + widthSegments, 1).forEach((j) => {
-                const x = vertices.getX(j);
-
-                const newY = Math.cos(theta * tRatio) * cubicon.functionDef(x);
-                const newZ = Math.sin(theta * tRatio) * cubicon.functionDef(x);
-
-                vertices.setY(j, newY);
-                vertices.setZ(j, newZ);
-            });
-        });
+        cubicon.setVertices(vertices, tRatio);
 
         vertices.needsUpdate = true;
     }
