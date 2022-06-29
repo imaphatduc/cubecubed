e221cbcdd7322134d028cb43a342d48db9f1252d
imaphatduc <vutranducphat@gmail.com>
2022-06-29 14:17:29 +0700

    refactor: `RevolutionSurface` refactored `setVertices()` method

--- src/canvas/cubicons/RevolutionSurface.ts
+++ src/canvas/cubicons/RevolutionSurface.ts
@@ -1,13 +1,6 @@
 import { range } from "d3-array";
 
-import {
-    BufferAttribute,
-    DoubleSide,
-    InterleavedBufferAttribute,
-    Mesh,
-    MeshNormalMaterial,
-    PlaneGeometry,
-} from "three";
+import { DoubleSide, Mesh, MeshNormalMaterial, PlaneGeometry } from "three";
 //+++++++++++++++++++++++++++++++++++++++++++++++++++//
 
 import { Vector3 } from "@math/vector";
@@ -125,15 +118,18 @@ export class RevolutionSurface extends CanvasCubicon {
         this.geometry.rotateX(0.5 * -Math.PI);
         this.geometry.translate(width / 2 + xRange[0], 0, 0);
 
-        const vertices = this.geometry.attributes.position;
-
-        this.setVertices(vertices);
+        this.setVertices(1);
     }
 
-    setVertices(
-        vertices: BufferAttribute | InterleavedBufferAttribute,
-        tRatio = 1
-    ) {
+    /**
+     * Set the surface's geometry based on the current animation stage.
+     *
+     * @param t "Eased" time changing smoothly in the range [0, 1].
+     * 0 for initial stage and 1 for the complete rendered mesh.
+     */
+    setVertices(t = 1) {
+        const vertices = this.geometry.attributes.position;
+
         const { numCurveVertices, numRevolVertices } = this.CONFIG;
 
         range(0, vertices.count).forEach((i) => {
@@ -146,8 +142,8 @@ export class RevolutionSurface extends CanvasCubicon {
 
             const x = vertices.getX(i);
 
-            const newY = Math.cos(theta * tRatio) * this.functionDef(x);
-            const newZ = Math.sin(theta * tRatio) * this.functionDef(x);
+            const newY = Math.cos(theta * t) * this.functionDef(x);
+            const newZ = Math.sin(theta * t) * this.functionDef(x);
 
             vertices.setY(i, newY);
             vertices.setZ(i, newZ);
