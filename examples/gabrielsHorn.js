1eaaf5e78cd1ff58890f54c2e652c0aaf24b407f
imaphatduc <vutranducphat@gmail.com>
2022-07-13 14:06:22 +0700

    Visualize Gabriel's horn with Cubecubed

---
+++ examples/gabrielsHorn.js
@@ -0,0 +1,51 @@
+import {
+    CanvasGroup,
+    DrawGridFromScreenSides,
+    Grid,
+    Group,
+    MathText,
+    RevolutionSurface,
+    Scene,
+    SimulateRevolution,
+    Vector2,
+    Write,
+} from "../src/index";
+
+function scenery() {
+    const scene = new Scene("three");
+
+    (() => {
+        const group = new Group("tex", scene);
+
+        const grid = new Grid({ group: group }).render();
+
+        const formula = new MathText({
+            group: group,
+            position: new Vector2(5, 3),
+            text: "y = \\frac{1}{x}",
+            fontSize: 30,
+        }).render();
+
+        group.play([new DrawGridFromScreenSides(grid)]);
+
+        group.play([new Write({ cubicon: formula, duration: 2000 })]);
+    })();
+
+    (() => {
+        const group = new CanvasGroup("three-group", scene);
+
+        const surface = new RevolutionSurface({
+            group: group,
+            functionDef: (x) => 1 / x,
+            CONFIG: {
+                xRange: [0.5, 5],
+            },
+        }).render();
+
+        group.play([
+            new SimulateRevolution({ cubicon: surface, duration: 2000 }),
+        ]);
+    })();
+}
+
+scenery();
