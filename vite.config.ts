77df193e9a1a6766968426ee9ee7376f8930f04d
imaphatduc <vutranducphat@gmail.com>
2022-07-20 11:21:11 +0700

    Vitest config: jsdom for client-side usage (dom)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-const path = require("path");
+import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+    test: {
+        globals: true,
+        environment: "jsdom",
+    },
     plugins: [tsconfigPaths()],
     build: {
         lib: {
