cdc6971f07d2f3f59478e693702a2cf5fe67910b
imaphatduc <vutranducphat@gmail.com>
2022-11-17 14:21:10 +0700

    feat: SimulateStream canvas animation

---
+++ src/canvas/animations/SimulateStream.ts
@@ -0,0 +1,40 @@
+import { CanvasAnimation, CanvasAnimationParams } from "./CanvasAnimation";
+
+import { StreamLine } from "@cubicons/StreamLine";
+
+export type SIMULATE_STREAM_TYPES = StreamLine;
+
+export class SimulateStream extends CanvasAnimation {
+    declare cubicon: SIMULATE_STREAM_TYPES;
+
+    constructor(params: CanvasAnimationParams<SIMULATE_STREAM_TYPES>) {
+        super({
+            cubicon: params.cubicon,
+
+            duration: params.duration,
+        });
+    }
+
+    /**
+     * @internal
+     */
+    play() {
+        this.simulateStream();
+    }
+
+    private simulateStream() {
+        const { vertices, maxVertices, points } = this.cubicon;
+
+        const nextVertex = this.cubicon.nextVertex();
+
+        vertices.push(nextVertex);
+
+        if (maxVertices > 0) {
+            if (vertices.length > maxVertices) {
+                vertices.shift();
+            }
+        }
+
+        this.cubicon.geometry.setFromPoints(points);
+    }
+}
